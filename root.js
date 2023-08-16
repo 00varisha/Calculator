6e2a32089e7e3c7ca08fa582aaad5efe07c66b74
Varisha <varishakhana@gmail.com>
2023-08-16 18:28:59 +0530

    Third

---
+++ root.js
@@ -0,0 +1,30 @@
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll(".buttons");
+const specialChars = ["%", "*", "/", "-", "+", "="];
+let output = "";
+
+const calculate = (btnValue) => {
+    if(btnValue === "=" && btnValue !== ""){
+       output = eval(output.replace("%", "/100"));
+    }else if(btnValue === "AC"){
+        output = "";
+    }else if(btnValue === "DEL"){
+        output = output.toString().slice(0, -1);
+    }else{
+        if(output === "" && specialChars.includes(btnValue)) return;
+        output += btnValue;
+    }
+    display.value = output;
+};
+
+buttons.forEach((button) => {
+
+    button.addEventListener("click", (e) =>
+    
+    {
+        if(!output && e.target.dataset.value === '='){
+            return alert('please add some calculation...');
+        }
+        calculate(e.target.dataset.value)
+    });
+});
